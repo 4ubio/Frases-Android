de2c2eeb666b7d38654bb3c9a2b9be38487253b5
4ubio <sebastianrubioquiroz@gmail.com>
2024-02-09 19:01:40 -0600

    Change Buttons

--- app/src/main/java/com/example/frases/MainActivity.kt
+++ app/src/main/java/com/example/frases/MainActivity.kt
@@ -8,16 +8,21 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,6 +33,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.frases.ui.theme.FrasesTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,22 +53,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-/*Nuevo Proyecto Frases*/
-/*Bajar Logo del IEST y Anahuac*/
-/*Colocarlos en res->drawable*/
-
-/*Logo del IEST lado superior derecho, fondo naranja*/
 @Composable
 fun QuoteScreen() {
+
+    var switch_logo by remember {mutableStateOf(true)}
+    var logo by remember {mutableStateOf(R.drawable.iest)}
+
+    var switch by remember {mutableStateOf(true)}
+    var newQuote by remember {mutableStateOf("Vince in bono malum")}
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFFFE5900))
     ) {
         Image(
-            painter = painterResource(id = R.drawable.iest),
+            painter = painterResource(id = logo),
             contentDescription = "Logo del IEST",
-            colorFilter = ColorFilter.tint(color = Color(0xFFFFFFFF)),
+            //colorFilter = ColorFilter.tint(color = Color(0xFFFFFFFF)),
             modifier = Modifier
                 .align(Alignment.TopEnd)
                 .padding(top = 20.dp, end = 20.dp)
@@ -85,11 +94,65 @@ fun QuoteScreen() {
                     fontWeight = FontWeight.Bold
                 )
                 Text(
-                    text = "Por la fe y la razón",
+                    text = newQuote,
                     color = Color(0xFFFFFFFF),
                     fontSize = 25.sp,
                     fontWeight = FontWeight.Bold
                 )
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+        ) {
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Button(
+                    onClick = {
+                        switch = !switch
+
+                        if (switch) {
+                            newQuote = "Vince in bono malum"
+                        } else {
+                            newQuote = "Por la fe y la razón"
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(Color(0xFFFFFFFF)),
+                    modifier = Modifier.padding(bottom = 30.dp)
+                ) {
+                    Text(
+                        text = "Cambiar frase",
+                        color = Color(0xFFFE5900),
+                        fontSize = 20.sp
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(10.dp))
+
+                Button(
+                    onClick = {
+                        switch_logo = !switch_logo
+
+                        if (switch_logo) {
+                            logo = R.drawable.iest
+                        } else {
+                            logo = R.drawable.anahuac
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(Color(0xFFFFFFFF)),
+                    modifier = Modifier.padding(bottom = 30.dp)
+                ) {
+                    Text(
+                        text = "Cambiar logo",
+                        color = Color(0xFFFE5900),
+                        fontSize = 20.sp
+                    )
+                }
             }
         }
     }
@@ -97,7 +160,7 @@ fun QuoteScreen() {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun QuotePreview() {
     FrasesTheme {
         QuoteScreen()
     }
